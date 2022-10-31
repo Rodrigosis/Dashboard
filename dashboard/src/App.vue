39f9e33eac343d94c3b40a09075664d75b053bc0
Rodrigo Souza <40663984+Rodrigosis@users.noreply.github.com>
2022-10-31 14:23:19 -0300

    first commit

---
+++ dashboard/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <main class="columns is-gapless is-multiline">
+      <div class="column is-1">
+        <CustomSideBar />
+      </div>
+      <div class="column is-11">
+        <AssetsTable />
+        <LineChart />
+      </div>
+    </main>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CustomSideBar from "@/components/CustomSideBar.vue";
+import AssetsTable from "@/components/AssetsTable.vue";
+import LineChart from "@/components/lineChart";
+
+export default defineComponent({
+  name: 'App',
+  components: {LineChart, AssetsTable, CustomSideBar},
+});
+</script>
+
+<style>
+
+</style>
---
+++ dashboard/src/components/CustomSideBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sidebar">
+    <h1>
+      <img src="../assets/logo.png" alt="">
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({name: "CustomSideBar"})
+</script>
+
+<style scoped>
+.sidebar {
+  padding: 1rem;
+  background-color: azure;
+  width: 100%;
+  height: 100vh;
+}
+
+@media only screen and (max-width: 768px){
+  .sidebar {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
